e6ff72d4c8d97c865ae1c610c03745eed7a0d872
Igor Balashov <igor4balashov@gmail.com>
2025-02-13 11:09:56 +0600

    env conf

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	config "url-shortener/internal/config"
+)
+
 func main() {
-	//todo
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	//todo: init logger: slog
+
+	//todo: init storage: sqlite
+
+	//todo: init router: chi, chi render
+
+	//todo: run server:
+
 }
